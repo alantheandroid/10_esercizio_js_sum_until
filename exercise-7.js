4548d4bb9213c488ebab045066732b2d83fbbce1
alantheandroid <alessio.ilardi@gmail.com>
2022-05-20 19:09:18 +0200

    feat: add 'maxValue' function

--- exercise-7.js
+++ exercise-7.js
@@ -1,5 +1,20 @@
 function sumUntil(maxValue) {
-  // ...
+  let numbers = [];
+  let baseNum = 1;
+
+  // scrittura dei numeri dentro l'array
+  do {
+    numbers.push(baseNum);
+    baseNum++;
+  } while (baseNum <= maxValue);
+
+  // somma dei numeri
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 }
+console.log(numbers);
 
 console.log(sumUntil(5));
